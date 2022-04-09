b82565cbcf5c9f7ad9c0d062d5f16fd9f5a75b89
Weslleyso <weslley16santos42@gmail.com>
2022-04-08 21:40:54 -0300

    Atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //caminho do seletor
+  templateUrl: './app.component.html', //template
+  styleUrls: ['./app.component.css'] // estilo CSS
 })
 export class AppComponent {
-  title = 'BlogPessoal';
-}
+  constructor (
+    public auth: AuthService
+  ){}
+} 
